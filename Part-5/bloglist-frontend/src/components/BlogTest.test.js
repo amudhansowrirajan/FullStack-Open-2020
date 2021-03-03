165a577ae653dd02224d6baf19f52e4fe6d8a801
amudhansowrirajan <amudhan.sowrirajan@gmail.com>
2021-03-03 09:15:18 +0530

    add comment

--- Part-5/bloglist-frontend/src/components/BlogTest.test.js
+++ Part-5/bloglist-frontend/src/components/BlogTest.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
 import { render, fireEvent } from "@testing-library/react";
+// import { prettyDOM } from '@testing-library/dom' // for displayingthe component in its entirity.
 import Blog from "./Blog";
 
 describe("BLOG testing ", () => {
